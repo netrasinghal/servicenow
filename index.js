8ccf9831a177f34d9d889a38c78733c550f91595
netrasinghal <netrasinghal20@gmail.com>
2020-08-26 12:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
-//const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
 
 const app=express()
 const port = process.env.PORT || 3000;
 const sn=require('servicenow-rest-api');
 const ServiceNow=new sn('dev60105','admin','XkAb39OonqOB');
-const ABOUT = "getdetails";
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,10 +12,9 @@ app.get('/',(request,response) =>{
 	res.status(200).send('Server is working')
 })
 
-app.post('/chatbot',(request,response) =>{
- const agent = new WebhookClient({ request, response });
- let callerid = request.body.queryResult.parameters.callerid;
-function aboutHandler(agent){
+app.post('/chatbot',(req,res) =>{
+ let callerid = req.body.queryResult.parameters.callerid;
+
 ServiceNow.Authenticate();
 const fields=[
     'number',
@@ -29,17 +25,12 @@ const filters=[
     'caller_idLIKE'+callerid
 ];
 
-ServiceNow.getTableData(fields,filters,'incident',function(res){
-    return res
-}).then((result)=>{
-	result.data.map(wordObj => {
-		agent.add(wordObj[0].number);		
-	});
-			});
-}
-let intentMap = new Map();
-  intentMap.set(ABOUT, aboutHandler);
-  agent.handleRequest(intentMap);
+ServiceNow.getTableData(fields,filters,'incident',function(result){
+ return res.json({
+                fulfillmentText: result,
+                source: 'chatbot'
+            })
+});
 });
 
 app.listen(port, () => { console.log(`Server running on port number: ${port}`) })
