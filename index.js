c3bd290b6fdd3de4a3dfe17699a61f4ae9f07f7e
netrasinghal <netrasinghal20@gmail.com>
2020-08-27 14:00:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//code for performing actions based on different agents
 const express = require('express')
 
 const app=express()
@@ -13,66 +14,83 @@ app.get('/',(request,response) =>{
 })
 
 app.post('/chatbot',(req,res) =>{
- let callerid = req.body.queryResult.parameters.callerid;
+ const intent = req.body.queryResult.intent.displayName;
 
-ServiceNow.Authenticate();
-const fields=[
-    'number',
-    'short_description',
-    'priority'
-];
-const filters=[
-    'caller_idLIKE'+callerid
-];
+if(intent=="getdetails"){
+	ServiceNow.Authenticate();
+	const fields=[
+	    'number',
+	    'short_description',
+	    'priority'
+	];
+	const filters=[
+	    'caller_idLIKE'+callerid
+	];
 
-ServiceNow.getTableData(fields,filters,'incident',function(result){
-var msg=[];
-for(var i in result){
-msg.push(result[i].number)
+	ServiceNow.getTableData(fields,filters,'incident',function(result){
+
+	if(result == ''){
+		var msg= "Sorry, there are no available incidents for this caller"
+	}
+
+	else{
+		var msg=[];
+		for(var i in result){
+		msg.push(result[i].number)
+	}
+	msg = "The tickets for this caller are: "+msg.toString()
+	}
+	 return res.json({
+	                fulfillmentMessages: [
+	      {
+	        "payload": {
+	          "richContent": [
+	            [
+	              {
+	                "type": "description",
+	                "title": msg,
+	                "text": [
+	                  "Please select the next action from the below options"
+	                ]
+	              },
+	              {
+	                "type": "chips",
+	                "options": [
+	                  {
+	                    "text": "Get details for a incident",
+	                    "link": ""
+	                  },
+	                  {
+	                    "link": "",
+	                    "text": "Create ticket"
+	                  },
+	                  {
+	                    "link": "",
+	                    "text": "Update ticket"
+	                  },
+	                  {
+	                    "text": "Delete ticket",
+	                    "link": ""
+	                  }
+	                ]
+	              }
+	            ]
+	          ]
+	        }
+	      }
+	    ],
+	                source: 'chatbot'
+	            })
+	});
 }
 
- return res.json({
-                fulfillmentMessages: [
-      {
-        "payload": {
-          "richContent": [
-            [
-              {
-                "type": "description",
-                "title": "The tickets for this caller are: "+msg.toString(),
-                "text": [
-                  "Please select the next action from the below options"
-                ]
-              },
-              {
-                "type": "chips",
-                "options": [
-                  {
-                    "text": "Get details for a incident",
-                    "link": ""
-                  },
-                  {
-                    "link": "",
-                    "text": "Create ticket"
-                  },
-                  {
-                    "link": "",
-                    "text": "Update ticket"
-                  },
-                  {
-                    "text": "Delete ticket",
-                    "link": ""
-                  }
-                ]
-              }
-            ]
-          ]
-        }
-      }
-    ],
-                source: 'chatbot'
-            })
-});
+else{
+	return res.json({
+            fulfillmentText: "Sorry, I cannot help with that.",
+            source: 'chatbot'
+        })
+}
+
 });
 
 app.listen(port, () => { console.log(`Server running on port number: ${port}`) })
